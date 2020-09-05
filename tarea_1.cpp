35e858b5e8d1a664f963b771435a1f5e875c4af5
Kyziak <asibarrao@unal.edu.co>
2020-09-05 11:41:15 -0500

    Se completó el cuadrado para la función modificada

--- tarea_1.cpp
+++ tarea_1.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
-#include <math.h>
-#include <cstdio>
-#include <iomanip>
-#include <limits>
+#include <cmath>
 
-using namespace std;
+typedef float real;
 
-int main()
-{
-    cout << "\t" << "_________________________________________________________________" << "\n" << endl;
-    cout << "\t" << "x" << "\t" << "\t" <<"\t"<< "f(x)" << "\t" << "\t" << "\t" << "Modificacion f(x)" << "\t" << endl;
-    cout << "\t" << "_________________________________________________________________" << "\n" << endl;
-    
-for (float i = 0; i <= 1; i = i + 0.01) {
-        cout << '\t' << scientific <<std::setprecision(3) << i << '\t' << '\t' << std::setprecision(6) << 5 - sqrt(25 + (i * i)) 
-        << '\t' << "\t"<< std::setprecision(8) <<5 - sqrt(25 + (i * i))<< '\t'
-        << endl; 
+const double xinit = 0.0;
+const double xend = 1.0;
+const double NSTEPS = 100;
+const double dx =  (xend - xinit)/NSTEPS;
+
+real funcion(real x);
+real funcionmod(real x);
+
+int main(void) {
+    std::cout.precision(8); std::cout.setf(std::ios::scientific);
+    real x = 0.0;
+    for (int ii = 0; ii < NSTEPS; ii++) {
+        x = xinit + ii*dx;
+        std::cout << ii*dx << " " << funcion(x) << " " << funcionmod(x) << "\n";
     }
     return 0;
+}
+
+real funcion(real x){
+    return 5 - std::sqrt(25 + x*x);
+}
+real funcionmod(real x){
+    return 5 - std::sqrt((5+x)*(5+x) - 10*x);
 }
